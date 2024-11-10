ba880a7f9c3df5d1ce8c1c5c3135085caf6b9b64
Yousuf0206 <yousuf_saleem@hotmail.com>
2024-11-10 23:38:56 +0500

    error removing

--- app/contact.tsx
+++ app/contact.tsx
@@ -40,7 +40,7 @@ const Contact = () => {
             <textarea
               id="message"
               name="message"
-              rows="4"
+              //rows="4"
               className="w-full px-4 py-2 text-gray-800 placeholder-gray-400 bg-gray-50 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
               placeholder="Type your message here..."
             />
